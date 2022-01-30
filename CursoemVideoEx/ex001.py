2f0ae5ff06138d6a6fbfb1f1efbb63b031b6916c
Eduardo Nogueira <edujsnogueira@gmail.com>
2022-01-29 22:14:22 -0300

    Commit inicial.

---
+++ CursoemVideoEx/ex001.py
@@ -0,0 +1,9 @@
+# deixando tudo pronto
+# apresentar na tela a mensagem 'Olá, Mundo!'
+
+# print('Olá, Mundo!')
+
+# alternativa criando uma variável.
+
+msg = 'Olá, Mundo!'
+print(msg)
---
+++ CursoemVideoEx/ex002.py
@@ -0,0 +1,9 @@
+# Faça um programa que leia o nome da pessoa e mostre uma mensagem de boas-vindas.
+
+# nome = input('Qual é o seu nome?')
+# print('Bem-vindo, ' + nome + '!')
+
+# Existe a opção de escrever o código com a saída formatada!
+
+nome = input('Qual é o seu nome?')
+print('É um prazer te conhecer, {}!'.format(nome))
