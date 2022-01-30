2a2c1aa4c3f16cfa5da1f6c92d29e6636f7b888e
Eduardo Nogueira <edujsnogueira@gmail.com>
2022-01-30 00:17:44 -0300

    Apagando duplicados.

--- CursoemVideoAulas/des001.py
+++
@@ -1,4 +0,0 @@
-# Desafio 001:
-
-nome = input('Qual é o seu nome?')
-print('Olá,', nome, '! Prazer em te conhecer!')
--- CursoemVideoAulas/des002.py
+++
@@ -1,6 +0,0 @@
-# Desafio 002:
-
-dia = input ('Qual dia você nasceu?')
-mes = input ('Qual mês você nasceu?')
-ano = input ('Qual ano você nasceu?')
-print ('Você nasceu no dia', dia, 'de', mes, 'de', ano, '! Correto?')
